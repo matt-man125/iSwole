070f55275f66965436e2efdb1533bb8dca2d6f2a
tylerklane <105730362+tylerklane@users.noreply.github.com>
2023-12-05 23:21:17 -0500

    Add files via upload

---
+++ iSwole-main/data/logs.js
@@ -0,0 +1,95 @@
+// This data file should export all functions using the ES6 standard as shown in the lecture code
+
+import { logs } from "../config/mongoCollections.js";
+import { ObjectId } from "mongodb";
+import validation from '../helpers.js';
+
+const exportedMethods = {
+  async create(
+    bodyWeight,
+    startTime,
+    endTime,
+    exerciseName0,
+    sets0,
+    reps0,
+    weight0,
+    exerciseName1,
+    sets1,
+    reps1,
+    weight1,
+    exerciseName2,
+    sets2,
+    reps2,
+    weight2,
+    exerciseName3,
+    sets3,
+    reps3,
+    weight3,
+    exerciseName4,
+    sets4,
+    reps4,
+    weight4
+  ) {
+    let newLog = {
+      bodyWeight: bodyWeight,
+      startTime: startTime,
+      endTime: endTime,
+      exerciseName0: exerciseName0,
+      sets0: sets0,
+      reps0: reps0,
+      weight0: weight0,
+      exerciseName1: exerciseName1,
+      sets1: sets1,
+      reps1: reps1,
+      weight1: weight1,
+      exerciseName2: exerciseName2,
+      sets2: sets2,
+      reps2: reps2,
+      weight2: weight2,
+      exerciseName3: exerciseName3,
+      sets3: sets3,
+      reps3: reps3,
+      weight3: weight3,
+      exerciseName4: exerciseName4,
+      sets4: sets4,
+      reps4: reps4,
+      weight4: weight4
+    };
+    const logCollection = await logs();
+    const insertInfo = await logCollection.insertOne(newLog);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId){
+      throw 'Could not add log';
+    }
+    return newLog;
+  },
+  
+  async getAll() {
+    const logCollection = await logs();
+    let logList = await logCollection.find({}).toArray();
+    if (!logList) throw 'Could not get all logs';
+    return logList;
+  },
+  
+  async get(logId) {
+    const logCollection = await logs();
+    const myLog = await logCollection.findOne({_id: new ObjectId(logId)});
+    if (myLog === null){
+      throw 'No log with that ID';
+    }
+    return myLog;
+  },
+  
+  async remove(logId) {
+    const logCollection = await logs();
+    const deletionInfo = await logCollection.findOneAndDelete({
+      _id: new ObjectId(logId)
+    });
+    if (deletionInfo.lastErrorObject.n === 0){
+      throw `Could not delete log wtih id of ${logId}`;
+    }
+    return `${deletionInfo.value.name} has been successfully deleted!`;
+  }
+};
+
+export default exportedMethods;
+
---
+++ iSwole-main/routes/logs.js
@@ -0,0 +1,51 @@
+// Import the express router as shown in the lecture code
+// Note: please do not forget to export the router!
+import {Router} from "express";
+const router = Router();
+import { logData } from "../data/index.js";
+import validation from '../helpers.js';
+import { logs } from "../config/mongoCollections.js";
+
+router
+  .route('/')
+  .get(async (req, res) => {
+    try{
+      res.render("log");
+    }catch(e){
+      res.status(500).json({error: e});
+    }
+  });
+
+router
+  .route('/progress')
+  .post(async (req, res) => {
+    const myLogData = req.body;
+    try{
+      const {bodyWeight, startTime, endTime, exerciseName0, sets0, reps0, weight0, exerciseName1, sets1, reps1, weight1, exerciseName2, sets2, reps2, weight2, exerciseName3, sets3, reps3, weight3, exerciseName4, sets4, reps4, weight4} = myLogData;
+      const newLog = await logData.create(bodyWeight, startTime, endTime, exerciseName0, sets0, reps0, weight0, exerciseName1, sets1, reps1, weight1, exerciseName2, sets2, reps2, weight2, exerciseName3, sets3, reps3, weight3, exerciseName4, sets4, reps4, weight4);
+      res.status(200).render('progress');
+    }catch(e){
+      res.status(400).json({error: e});
+    }
+  });
+
+router
+  .route('/:logId')
+  .get(async (req, res) => {
+    try{
+      const log = await logData.get(req.params.logId);
+      res.status(200).json(log);
+    }catch(e){
+      return res.status(404).json({error: e});
+    }
+  })
+  .delete(async (req, res) => {
+    try{
+      let deletedLog = await logData.remove(req.params.logId);
+      res.status(200).json({logId: req.params.logId, deleted: true});
+    }catch(e){
+      return res.status(404).json({error: e});
+    }
+  });
+
+export default router;
